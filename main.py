1cafea1bb3d70be80e5e3db5faf65ad0972c83c1
wolvfang <wolvfang@users.noreply.github.com>
2018-05-20 15:21:46 +0100

    code submission

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from settings import settings
 
@@ -10,6 +10,14 @@ app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 @app.route("/")
 def index():
     return render_template('index.pug')
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    json_data = request.json
+    print(json_data)
+    # TODO: save data to database
+    return ""
 
 
 if __name__ == "__main__":
---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+var btn = document.getElementById("submit");
+var title = document.getElementById("title");
+var code = document.getElementById("code");
+var language = document.getElementById("language");
+var xhr = new XMLHttpRequest()
+
+btn.addEventListener('click', submitCode);
+
+
+function submitCode()
+{
+    if (title.value === "")
+    {
+        alert("Please enter a title!");
+        return;
+    }
+
+    if (code.value === "")
+    {
+        alert("Please enter your code!");
+        return;
+    }
+
+    var lang_input = language.value;
+    var title_input = title.value;
+    var code_input = code.value;
+
+    xhr.open("POST", "/submit", true);
+    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8")
+    xhr.send('{"language": "' + lang_input + '", "title": "' + title_input + '", "code": "' + code_input + '"}')
+}
